52d359ec156997fb7e89a43bfc15d5bf56ae0354
burakerdgn1 <be871885@gmail.com>
2021-08-17 17:24:22 +0300

    Update
    Sign in dialog has been started do be designed
    Dropdown menu featured image has been added.
    Js functionalities have been started to be added.

---
+++ script.js
@@ -0,0 +1,18 @@
+let btn= document.querySelector('#login');
+let div= document.querySelector('#appear');
+
+btn.addEventListener('click',()=>{
+    if(div.style.display==='none'){
+        div.style.display='block';
+    }
+    else{
+        div.style.display='none';
+    }
+
+});
+
+let exit= document.querySelector('#esc');
+exit.addEventListener('click',()=>{
+    div.style.display='none';
+
+});
